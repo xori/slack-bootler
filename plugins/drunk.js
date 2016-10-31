e1beb42724592352fc62f49878a72b5b8c842a7f
Evan Verworn <evan.verworn@dsbn.org>
2016-10-31 15:22:31 -0400

    WIP

---
+++ plugins/drunk.js
@@ -0,0 +1,16 @@
+/**
+ * Party time for Bootler.
+ * 
+ * - Allow people to give him beers/shots
+ * - After a threshold on a timer have him send drunk texts and post party gifs
+ *  - http://textsfromlastnight.com/Random-Texts-From-Last-Night.html
+ * - Have him throwup (if really drunk), sleep for 8 hours (not responding to anyone), and wake up.
+ *  - Go home bootler, you're drunk
+ */
+module.exports = function(bot) {
+  bot.respond(/have (?:a|some) (?:beer|wine|)/i, function(message, params, send) {
+    
+  });
+
+  
+}

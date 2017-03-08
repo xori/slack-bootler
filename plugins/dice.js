dc5c3ba6df3fc2c325ca3551c18bbfc98b2e1663
Evan Verworn <xori@users.noreply.github.com>
2017-03-08 13:34:10 -0500

    Prevent rolling of dice when you aren't trying to roll dice
    ```
    evan> we just saw someone's porch roof roll down the road
    bootler> APP d isn't in standard dice format.
    evan> -_-
    ```

--- plugins/dice.js
+++ plugins/dice.js
@@ -1,7 +1,7 @@
 const R = require('roll');
 
 module.exports = function(engine) {
-  engine.on(/(?:^|\s)roll ([0-9d\s*\/+\-]+)/i, function(message, params, send) {
+  engine.on(/(?:^|\s)roll ([1-9]*d[0-9d\s*\/+\-]+)/i, function(message, params, send) {
     let ask = params[1].split(" ");
     let dice = new R();
 

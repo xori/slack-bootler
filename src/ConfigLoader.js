3ef1b100595bf38aff0d0b7272488a04b7dc1059
Evan Verworn <evan.verworn@dsbn.org>
2019-07-11 10:52:28 -0400

    disable karping

--- src/ConfigLoader.js
+++ src/ConfigLoader.js
@@ -13,6 +13,7 @@ try {
 config.token = process.env.SLACK_TOKEN || config.token;
 config.admintoken = process.env.SLACK_ADMINTOKEN || config.admintoken;
 config.google_api = process.env.GOOGLE_API || config.google_api;
+config.lcbo = process.env.LCBO_API || config.lcbo;
 config.brain = process.env.SLACK_BRAIN || config.brain || "./brain";
 config.wolfram = process.env.WOLFRAM || config.wolfram;
 config.host = process.env.SLACK_HOSTNAME || config.host || "http://localhost:8000";
